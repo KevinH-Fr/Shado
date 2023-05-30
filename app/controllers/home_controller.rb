239ee52457f4f637e73d28dc6c39879d3fd266ed
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-05-30 20:39:04 +0200

    modifs btns navbar et alternative small screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,22 @@ class HomeController < ApplicationController
   include UsersHelper 
 
   def index
-    @athlete = Athlete.where(user_id: current_user)
+    @athlete = Athlete.where(user_id: current_user).first
     
     if user_role(current_user.id) == "fan" 
       @partial_role = 'fan_infos'
       @fan = Fan.find(user_role_id(current_user))
       @athletes = @fan.athletes.distinct
+
+      @posts = Post.non_exclusif
+      @posts_suivis = Post.where(athlete_id: @athletes.pluck(:id))
+
+
     elsif user_role(current_user) == "athlete" 
       @partial_role = 'athlete_infos'
+
+      @campaigns = @athlete.campaigns
+      @followers = Fan.all
     end
 
   end
