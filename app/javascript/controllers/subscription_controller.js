303c877653b7cb5e87457cd37021630b8af7fb74
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-06-19 22:32:38 +0200

    avancement controller subscription js pour calcul cover the fees

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -3,27 +3,47 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   // champs dans le form resultat
-  static targets = [   
-     "prixBrutInitial", "prixInitial"
-  ];
+  static targets = ["prixBrutInitial", "prixNetInitial", "prixInitial", "spanTotal", "spanNet", "spanPlatformFeesInitial",
+                    "platformFeesInitial", "paymentFeesInitial"];
 
-  coverFees(event) {
+  toggleFees(event) {
     event.preventDefault();
-    this.prixInitialTarget.value =  this.prixInitialTarget.value * 1.05;
-    console.log("test click cover")
+    const button = event.currentTarget;
+    const totalSpan = this.spanTotalTarget;
+    const netSpan = this.spanNetTarget;
+    const platformFeesInitialSpan = this.spanPlatformFeesInitialTarget;
 
+    if (totalSpan.textContent === this.prixBrutInitialTarget.value) {
+      this.prixInitialTarget.value = this.prixInitialTarget.value * 1.05;
+      button.textContent = "Uncover the fees";
+      totalSpan.textContent =parseFloat(this.prixInitialTarget.value).toFixed(2);
+      netSpan.textContent = parseFloat(this.prixBrutInitialTarget.value).toFixed(2);
+      platformFeesInitialSpan.textContent = this.platformFeesInitialTarget.value;
+
+    } else {
+      this.prixInitialTarget.value = this.prixBrutInitialTarget.value;
+      button.textContent = "Cover the fees";
+      totalSpan.textContent = parseFloat(this.prixBrutInitialTarget.value).toFixed(2);
+      netSpan.textContent = parseFloat(this.prixNetInitialTarget.value).toFixed(2);
+      platformFeesInitialSpan.textContent = (this.prixInitialTarget.value * 1.05) * 0.1;
+
+    }
   }
-
-  uncoverFees(event) {
-    event.preventDefault();
-    this.prixInitialTarget.value = this.prixBrutInitialTarget.value;
-    console.log("test click uncover")
-
-  }
+  
 
   connect() {
     console.log("test connect controller sub")
     console.log("intial brut prix: " +  this.prixBrutInitialTarget.value )
+    console.log("platform fee ini: " +  this.platformFeesInitialTarget.value )
+
+    const totalSpan = document.getElementById("span-total");
+    const netSpan = this.spanNetTarget;
+
+    const platformFeesInitialSpan = this.platformFeesInitialTarget;
+    platformFeesInitialSpan.textContent = this.platformFeesInitialTarget.value;
+
+    totalSpan.textContent = parseFloat(this.prixBrutInitialTarget.value).toFixed(2);
+    netSpan.textContent = parseFloat(this.prixNetInitialTarget.value).toFixed(2);
 
   }
 
